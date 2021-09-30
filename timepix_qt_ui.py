07433710168e927027e9a691226a1d37402681cd
Ryan Lee <ryanl@berkeley.edu>
2021-09-30 16:44:53 -0700

    Added Viewer controls to UI panel

--- timepix_qt_ui.py
+++ timepix_qt_ui.py
@@ -15,12 +15,16 @@ import queue
 
 import tp4
 
-
-class TimepixControlToolbar(QWidget):
-    frameChanged = pyqtSignal(int) #emits when there is a new frame selected
-
+class QHLine(QFrame):
     def __init__(self):
-        super(TimepixControlToolbar, self).__init__()
+        super(QHLine, self).__init__()
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
+
+class TP4ImageViewControl(QWidget):
+
+    def __init__(self, autoLevel=True):
+        super(TP4ImageViewControl, self).__init__()
         self.setFixedWidth(350)
         toolbarLayout = QVBoxLayout()
 
@@ -28,43 +32,62 @@ class TimepixControlToolbar(QWidget):
         frameToolLayout = QHBoxLayout()
         frameToolLayout.addWidget(QLabel("Frame"))
         self.frameCounterTool = QSpinBox()
-        self.frameCounterTool.valueChanged.connect(self._emit_frameChanged)
         frameToolLayout.addWidget(self.frameCounterTool)
 
-        self.updateFrameButton = QPushButton("Update")
-        frameToolLayout.addWidget(self.updateFrameButton)
+        self.updateFrame = QPushButton("Update")
+        frameToolLayout.addWidget(self.updateFrame)
 
-        self.frameIncrement = QPushButton("<")
+        self.frameDecrement = QPushButton("<")
+        self.frameDecrement.setFixedWidth(30)
         self.frameIncrement = QPushButton(">")
+        self.frameIncrement.setFixedWidth(30)
         #frameToolLayout.addWidget()
-        frameToolLayout.addWidget(QPushButton(">"))
+        frameToolLayout.addWidget(self.frameDecrement)
+        frameToolLayout.addWidget(self.frameIncrement)
         toolbarLayout.addLayout(frameToolLayout)
         
+        toolbarLayout.addWidget(QHLine())
         
         minLevelLayout = QGridLayout()
         minLevelLayout.addWidget(QLabel("Min Level:"), 0, 0)
-        minLevelLayout.addWidget(QCheckBox("Lock"), 1, 0)
-        minLevelLayout.addWidget(QTextEdit(), 0, 1)
-        minLevelLayout.addWidget(QSlider(Qt.Horizontal), 1, 1)
+        self.minLevelLock = QCheckBox("Lock")
+        minLevelLayout.addWidget(self.minLevelLock, 1, 0)
+        self.minLevel = QTextEdit()
+        self.minLevel.setFixedHeight(28)
+        minLevelLayout.addWidget(self.minLevel, 0, 1)
+        self.minLevelSlider = QSlider(Qt.Horizontal)
+        minLevelLayout.addWidget(self.minLevelSlider, 1, 1)
         minLevelLayout.addWidget(QPushButton("Under\n Warning"), 0, 2)
         toolbarLayout.addLayout(minLevelLayout)
 
+        toolbarLayout.addWidget(QHLine())
+
         minLevelLayout = QGridLayout()
         minLevelLayout.addWidget(QLabel("Min Level:"), 0, 0)
-        minLevelLayout.addWidget(QCheckBox("Lock"), 1, 0)
-        minLevelLayout.addWidget(QTextEdit(), 0, 1)
-        minLevelLayout.addWidget(QSlider(Qt.Horizontal), 1, 1)
+        self.maxLevelLock = QCheckBox("Lock")
+        minLevelLayout.addWidget(self.maxLevelLock, 1, 0)
+        self.maxLevel = QTextEdit()
+        self.maxLevel.setFixedHeight(28)
+        minLevelLayout.addWidget(self.maxLevel, 0, 1)
+        self.maxLevelSlider = QSlider(Qt.Horizontal)
+        minLevelLayout.addWidget(self.maxLevelSlider, 1, 1)
         minLevelLayout.addWidget(QPushButton("Over\n Warning"), 0, 2)
         toolbarLayout.addLayout(minLevelLayout)
 
+        toolbarLayout.addWidget(QHLine())
+
         graphControlLayout = QGridLayout()
-        graphControlLayout.addWidget(QCheckBox("Auto Range:"), 0, 0)
+        self.autoLevel = QCheckBox("Auto Range:")
+        self.autoLevel.setChecked(autoLevel)
+        graphControlLayout.addWidget(self.autoLevel, 0, 0)
         graphControlLayout.addWidget(QComboBox(), 0, 1)
         graphControlLayout.addWidget(QCheckBox("Count Rate"), 1, 0)
         graphControlLayout.addWidget(QLabel("Time:"), 1, 1)
         graphControlLayout.addWidget(QCheckBox("Histogram: "), 2, 0)
         graphControlLayout.addWidget(QPushButton("Auto refine"), 2, 1)
         toolbarLayout.addLayout(graphControlLayout)
+
+        toolbarLayout.addWidget(QHLine())
         
         graphButtonLayout = QHBoxLayout()
         graphButtonLayout.addWidget(QPushButton("<-"))
@@ -90,8 +113,17 @@ class TimepixControlToolbar(QWidget):
         toolbarLayout.addWidget(QCheckBox("Auto Update Preview"))
         self.setLayout(toolbarLayout)
     
-    def _emit_frameChanged(self, newFrame):
-        self.frameChanged.emit(newFrame)
+    def setLevels(minLevel, maxLevel, levelRange):
+        print(minLevel, maxLevel, levelRange)
+
+    def update(self):
+        isAutoLevel = self.autoLevel.isChecked()
+        self.minLevel.setDisabled(isAutoLevel)
+        self.minLevelSlider.setDisabled(isAutoLevel)
+        self.minLevelLock.setDisabled(isAutoLevel)
+        self.maxLevel.setDisabled(isAutoLevel)
+        self.maxLevelSlider.setDisabled(isAutoLevel)
+        self.maxLevelLock.setDisabled(isAutoLevel)
 
     def _create_Widgets():
         pass
@@ -102,27 +134,24 @@ class TimepixControlToolbar(QWidget):
 class TimePixImageFetcher(QThread):
     imageUpdated = pyqtSignal(np.ndarray)
 
-    def __init__(self, imgViewer : pg.ImageView, host="127.0.0.1", port=2686, buffer_size=1000):
+    def __init__(self, imgViewer : pg.ImageView, host="127.0.0.1", port=2686, fps=60):
         super().__init__()
         self.imgViewer = imgViewer
 
-        self.packet_buffer = queue.Queue(maxsize=buffer_size)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.img = np.zeros(512*512)
-
-
         self.sock.bind((host, port))
+
         self.counter = 0
-        self.integrate_mode = 1
+        self.integrate_mode = 0
+        self.fps = fps
     
     def run(self):
         while True:
             packet = self.sock.recvfrom(tp4.PACKET_SIZE)
-            
 
             for i in range(tp4.HEADER_SIZE,len(packet[0])):
                 self.img[(self.counter*128*128) + i - tp4.HEADER_SIZE] += packet[0][i]
-
             self.counter += 1
 
             if self.counter >= 16:
@@ -131,7 +160,7 @@ class TimePixImageFetcher(QThread):
                 if not self.integrate_mode:
                     self.img = np.zeros(512*512)
                 self.counter = 0
-                time.sleep(0.016)
+                time.sleep(1/self.fps)
 
 class TimepixControl(QMainWindow):
     """Main Window"""
@@ -147,7 +176,6 @@ class TimepixControl(QMainWindow):
 
         #Create Image Viewer on Left
         self.imgViewer = pg.ImageView()
-        
         #self.imgViewer.ui.histogram.hide()
         self.imgViewer.ui.roiBtn.hide()
         self.imgViewer.ui.menuBtn.hide()
@@ -157,11 +185,9 @@ class TimepixControl(QMainWindow):
         self.tp4_image_fetcher.imageUpdated.connect(self.updateImageViewer)
         self.tp4_image_fetcher.start()
 
-
-        self.toolbar = TimepixControlToolbar()
-        #self.toolbar.printTestSignal.connect(self.printTest)
+        self.imgViewerControl = TP4ImageViewControl()
         
-        layout.addWidget(self.toolbar)
+        layout.addWidget(self.imgViewerControl)
         window.setLayout(layout)
 
         self.setCentralWidget(window)
@@ -172,7 +198,13 @@ class TimepixControl(QMainWindow):
     start_time = time.time()
 
     def updateImageViewer(self, img):
-        self.imgViewer.setImage(img, autoRange=False)
+        self.imgViewer.setImage(img, autoRange=False, autoLevels=self.imgViewerControl.autoLevel.isChecked())
+
+        #self.imgViewerControl.setLevels(self.imgViewer.levelMin, self.imgViewer.levelMax, levelRange=self.imgViewer.quickMinMax(self.imgViewer.imageDisp))
+
+        self.imgViewerControl.update()
+        self.imgViewerControl.minLevel.setPlainText(str(self.imgViewer.levelMin))
+        self.imgViewerControl.maxLevel.setPlainText(str(self.imgViewer.levelMax))
 
     def _createActions(self):
         #Actions for the File Menu
